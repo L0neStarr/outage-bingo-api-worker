ddf8444e6de6a84ec4cf47faca1cb9f969e5266f
L0neStarr <perezj7991@gmail.com>
2026-01-01 20:41:18 -0500

    Adding bingo card building logic

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,15 @@
-/**
- * Welcome to Cloudflare Workers!
- *
- * This is a template for a Scheduled Worker: a Worker that can run on a
- * configurable interval:
- * https://developers.cloudflare.com/workers/platform/triggers/cron-triggers/
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Run `curl "http://localhost:8787/__scheduled?cron=*+*+*+*+*"` to see your Worker in action
- * - Run `npm run deploy` to publish your Worker
- *
- * Bind resources to your Worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { connect } from "cloudflare:sockets";
+
+interface Env {
+	BINGO_BUCKET: R2Bucket;
+}
+
+function padMM(mm:number) {
+	return String(mm).padStart(2, "0")
+}
+
+
+
 
 export default {
 	async fetch(req) {
@@ -21,20 +17,33 @@ export default {
 		url.pathname = '/__scheduled';
 		url.searchParams.append('cron', '* * * * *');
 		return new Response(`To test the scheduled handler, ensure you have used the "--test-scheduled" then try running "curl ${url.href}".`);
-	},
+	}, // async fetch
 
 	// The scheduled handler is invoked at the interval set in our wrangler.jsonc's
 	// [[triggers]] configuration.
-	async scheduled(event, env, ctx): Promise<void> {
-		// A Cron Trigger can make requests to other endpoints on the Internet,
-		// publish to a Queue, query a D1 Database, and much more.
-		//
-		// We'll keep it simple and make an API call to a Cloudflare API:
-		let resp = await fetch('https://api.cloudflare.com/client/v4/ips');
-		let wasSuccessful = resp.ok ? 'success' : 'fail';
+	async scheduled(controller, env, ctx): Promise<void> {
 
-		// You could store this result in KV, write to a D1 Database, or publish to a Queue.
-		// In this template, we'll just log the result:
-		console.log(`trigger fired at ${event.cron}: ${wasSuccessful}`);
-	},
-} satisfies ExportedHandler<Env>;
+		switch (controller.cron) {
+
+			case "0 0 1 * *":
+				//Build monthly bingo card
+				const date = new Date()
+				const yyyy = date.getUTCFullYear()
+				const mm = padMM(date.getUTCMonth() + 1)
+				const key = `outages-${yyyy}-${mm}.json`
+				
+				if(!await env.BINGO_BUCKET.get(key)) {
+				const template =  await env.BINGO_BUCKET.get(`outages-template.json`)
+				if(!template) throw new Error(`Missing Key`)
+				await env.BINGO_BUCKET.put(key, template.body, {httpMetadata: { contentType: "application/json" }})
+				}
+
+
+				break;
+			case  "* 1 * * *":
+				// Hourly API call
+				break;
+		}
+
+	}, // async scheduled
+} satisfies ExportedHandler<Env>; // export default
